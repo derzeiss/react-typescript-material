2a4ecae91c165a6c999a6739378b4b716bb9898e
alex-at-dev <123548703+alex-at-dev@users.noreply.github.com>
2024-08-16 12:48:32 +0200

    Syntax housekeeping

--- react-app/src/utils/cx.ts
+++ react-app/src/utils/cx.ts
@@ -3,16 +3,14 @@
  * @example cx('one', 'two three   ', {'four': false, 'five': 3 > 2}) -> 'one two three five'
  * @returns className string
  */
-export const cx = (
-  ...args: (string | undefined | { [cls: string]: boolean })[]
-) => {
+export const cx = (...args: (string | undefined | Record<string, boolean>)[]) => {
   const cls = args.reduce((cls: string, arg) => {
     if (!arg) return cls;
-    if (typeof arg === "string") return cls + " " + arg;
+    if (typeof arg === 'string') return cls + ' ' + arg;
     Object.entries(arg).forEach(([c, shouldApply]) => {
-      if (shouldApply) cls += " " + c;
+      if (shouldApply) cls += ' ' + c;
     });
     return cls;
-  }, "");
-  return cls.replace(/\s{2,}/g, " ").replace(/(^\s+|\s+$)/g, "");
+  }, '');
+  return cls.replace(/\s{2,}/g, ' ').replace(/(^\s+|\s+$)/g, '');
 };
